0c35e777bdef702619a16bf4986e59d018159f85
Leonard <weasleywalker@gmx.de>
2024-07-15 15:02:21 +0200

    fixed null pointer exception bug

--- Assets/Scripts/Tower/Projectile.cs
+++ Assets/Scripts/Tower/Projectile.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [Header("References")]
-    [SerializeField]
-    private Rigidbody2D rb;
+    [SerializeField] private Rigidbody2D rb;
 
     [Header("Attributes")]
     [SerializeField] private float projectileSpeed = 5f;
@@ -14,24 +13,26 @@ public class Projectile : MonoBehaviour
 
     private Transform target;
 
-    public void SetTarget(Transform _target)
+    public void SetTarget(Transform target)
     {
-        target = _target;
+        this.target = target;
     }
 
     private void FixedUpdate()
     {
         if (!target) return;
-
-        Vector2 direction = (target.position - transform.position).normalized; //normalized = nur zwischen 0 und 1
-       
+        //normalized = nur zwischen 0 und 1
+        Vector2 direction = (target.position - transform.position).normalized; 
         rb.velocity = direction * projectileSpeed;
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        other.gameObject.GetComponent<EnemyHealth>().TakeDamage(projectileDamage);
-        Destroy(gameObject);
-
+        if (other.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Hit");
+            other.gameObject.GetComponent<EnemyHealth>().TakeDamage(projectileDamage);
+            Destroy(gameObject);
+        }
     }
 }

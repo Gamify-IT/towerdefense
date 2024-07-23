6acaaea84fe9534fcc9cb28d2f8f6aa4024d6f8c
SuyaneKurz <st185451@stud.uni-stuttgart.de>
2024-07-23 18:25:32 +0200

    Documentation for QuestionHandler script

--- Assets/Scripts/Tower/QuestionHandler.cs
+++ Assets/Scripts/Tower/QuestionHandler.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+/// <summary>
+///  This class makes sure you can't place a tower while answering a question
+/// </summary>
 public class QuestionHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public bool mouse_over = false;
 
 
-
+    /// <summary>
+    ///  If the mouse hovers over the QuestionUI towers can#t be build
+    /// </summary>
+    /// <param name="eventData"> the mouse</param>
     public void OnPointerEnter(PointerEventData eventData)
     {
         mouse_over = true;
         UIManager.main.SetHoveringState(true);
     }
 
+    /// <summary>
+    ///  If the mouse is not hovering over the QuestionUI it closes again
+    /// </summary>
+    /// <param name="eventData"> the mouse </param>
     public void OnPointerExit(PointerEventData eventData)
     {
         mouse_over = false;

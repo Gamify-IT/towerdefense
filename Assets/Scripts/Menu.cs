366b17096217cd164784814c55c50d553f0987d3
SuyaneKurz <st185451@stud.uni-stuttgart.de>
2024-07-23 17:25:53 +0200

    Update Menu script documentation

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.EventSystems;
-
+/// <summary>
+///  This class handles the ingame menu 
+/// </summary>
 public class Menu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 { 
 
@@ -15,11 +17,18 @@ public class Menu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 
     public bool mouse_over = false;
 
+    /// <summary>
+    ///  This function handles opening the menu
+    /// </summary>
     public void ToggleMenu()
     {
         isMenuOpen = !isMenuOpen;
         anim.SetBool("MenuOpen", isMenuOpen);
     }
+
+    /// <summary>
+    ///  This function displays the current in game currency amount
+    /// </summary>
 
     private void OnGUI()
     {
@@ -27,16 +36,29 @@ public class Menu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 
     }
 
+
+    /// <summary>
+    ///  This function is used in Unity to select the tower
+    /// </summary>
     public void SetSelected()
     {
 
     }
+
+    /// <summary>
+    ///  This function sets the setHoveringState function to true if the mouse is over the menu
+    /// </summary>
+    /// <param name="eventData"> The mouse</param>
     public void OnPointerEnter(PointerEventData eventData)
     {
         mouse_over = true;
         UIManager.main.SetHoveringState(true);
     }
 
+    /// <summary>
+    ///  This function sets the setHoveringState function to false if the mouse is over the menu
+    /// </summary>
+    /// <param name="eventData"> The mouse</param>
     public void OnPointerExit(PointerEventData eventData)
     {
         mouse_over = false;

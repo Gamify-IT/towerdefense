af54ab5ac6118881bd811845f96e746e5f36a61d
SuyaneKurz <st185451@stud.uni-stuttgart.de>
2024-07-23 17:31:22 +0200

    documentation for LevelManager script

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+///  This class manages the basic functions of the levels
+/// </summary>
 public class LevelManager : MonoBehaviour
 {
     public static LevelManager main;
@@ -21,11 +23,19 @@ public class LevelManager : MonoBehaviour
         currency = 100;
     }
 
+    /// <summary>
+    ///  This function increases the currency
+    /// </summary>
+    /// <param name="amount"> the amount of how much the currency is increased</param>
     public void GainCurrency(int amount)
     {
         currency += amount;
     }
 
+    /// <summary>
+    ///  This function decreases the currency
+    /// </summary>
+    /// <param name="amount"> the amount of how much the currency is decreased</param>
     public bool SpendCurrency(int amount)
     {
         if(amount  <= currency) {

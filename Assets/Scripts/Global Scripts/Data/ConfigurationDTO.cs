7c7f2bf45c1e233d7c78f1920c6650a407a6940a
Leonard <weasleywalker@gmx.de>
2024-08-26 17:06:26 +0200

    added configuration classes

---
+++ Assets/Scripts/Global Scripts/Data/ConfigurationDTO.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///     This class is used to retrieve question data from get requests.
+/// </summary>
+[Serializable]
+public class ConfigurationDTO
+{
+    public QuestionDTO[] questions;
+
+    public ConfigurationDTO(QuestionDTO[] questions)
+    {
+        this.questions = questions;
+    }
+
+    /// <summary>
+    ///     This function converts a <c>ConfigurationData</c> object into a <c>ConfigurationDTO</c> instance
+    /// </summary>
+    /// <param name="data">The <c>ConfigurationData</c> object to convert</param>
+    /// <returns>The <c>ConfigurationDTO</c> instance</returns>
+    public static ConfigurationDTO ConvertDataToDTO(ConfigurationData data)
+    {
+        QuestionDTO[] questions = new QuestionDTO[data.GetQuestions().Length];
+
+        for (int i = 0; i < questions.Length; i++)
+        {
+            questions[i] = QuestionDTO.ConvertDataToDTO(data.GetQuestions()[i]);
+        }
+
+        return new ConfigurationDTO(questions);
+    }
+
+    /// <summary>
+    ///     This function converts a json string to a <c>ConfigurationDTO</c> object.
+    /// </summary>
+    /// <param name="jsonString">The json string to convert</param>
+    /// <returns>A <c>ConfigurationDTO</c> object containing the data</returns>
+    public static ConfigurationDTO CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<ConfigurationDTO>(jsonString);
+    }
+
+}
--- Assets/Scripts/Global Scripts/Data/ConfigurationData.cs
+++ Assets/Scripts/Global Scripts/Data/ConfigurationData.cs
@@ -7,5 +7,38 @@ using System;
 [Serializable]
 public class ConfigurationData
 {
-    private QuestionDTO[] questions;
+    private QuestionData[] questions;
+
+    public ConfigurationData(QuestionData[] questions)
+    {
+        this.questions = questions;
+    }
+
+    /// <summary>
+    ///     This function converts a <c>ConfigurationDTO</c> object into a <c>ConfigurationData</c> instance
+    /// </summary>
+    /// <param name="dto">The <c>ConfigurationDTO</c> object to convert</param>
+    /// <returns>The <c>ConfigurationData</c> instance</returns>
+    public static ConfigurationData ConvertDtoToData(ConfigurationDTO dto)
+    {
+        QuestionData[] questions = new QuestionData[dto.questions.Length];
+
+        for (int i = 0; i < questions.Length; i++)
+        {
+            questions[i] = QuestionData.ConvertDtoToData(dto.questions[i]);
+        }
+
+        return new ConfigurationData(questions);
+    }
+
+    public QuestionData[] GetQuestions()
+    {
+        return questions;
+    }
+
+    public void SetQuestions(QuestionData[] questions)
+    {
+        this.questions = questions;
+    }
+
 }

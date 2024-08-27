d02c3cd9277416bafb84579fe9e878939488c503
SuyaneKurz <st185451@stud.uni-stuttgart.de>
2024-08-27 15:39:03 +0200

    Toggle speed
    Speed of the gameplay can be doubled now (switch back not possible at the moment)

--- Assets/Scripts/HUD/PauseButton.cs
+++ Assets/Scripts/HUD/PauseButton.cs
@@ -5,13 +5,43 @@ using UnityEngine.SceneManagement;
 
 public class PauseButton : MonoBehaviour
 {
-   /// <summary>
-   /// Pauses the game and loads the pause menu
-   /// </summary>
-   /// <param name="sceneID"> the ID of the Pause menu scene</param>
+    [Header("References")]
+    public GameObject normalSpeedButton; 
+    public GameObject fastSpeedButton;
+
+
+    private bool isFast = false;
+
+    /// <summary>
+    /// Pauses the game and loads the pause menu
+    /// </summary>
+    /// <param name="sceneID"> the ID of the Pause menu scene</param>
     public void Pause(int sceneID)
     {
         Time.timeScale = 0f;
         SceneManager.LoadScene(sceneID, LoadSceneMode.Additive);
+    }
+
+    /// <summary>
+    /// Toggles the game speed between normal (1x) and fast (2x)
+    /// </summary>
+    public void ToggleSpeed()
+    {
+        if (isFast)
+        {
+            Time.timeScale = 1f; 
+            isFast = false;
+
+            normalSpeedButton.SetActive(true);
+            fastSpeedButton.SetActive(false);
+        }
+        else
+        {
+            Time.timeScale = 2f; 
+            isFast = true;
+
+            normalSpeedButton.SetActive(false);
+            fastSpeedButton.SetActive(true);
+        }
     }
 }
